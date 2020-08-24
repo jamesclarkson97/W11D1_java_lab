77bba4753ea71fd69def85f365ec188fa7e1ed33
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-24 15:25:54 +0100

    check water bottle volume passed

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,11 @@
+public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+}
---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before() {
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void checkVolume() {
+        assertEquals(100, waterBottle.getVolume());
+    }
+}
