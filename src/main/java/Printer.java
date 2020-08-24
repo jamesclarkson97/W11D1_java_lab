809d3ad26dbb04a043353f48af65e26d578195a9
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-24 16:11:38 +0100

    added toner property and get test passed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,12 +1,27 @@
 public class Printer {
     private int sheets;
+    private int toner;
 
-    public Printer(int sheets) {
+    public Printer(int sheets, int toner) {
         this.sheets = sheets;
+        this.toner = toner;
     }
 
     public int getSheets() {
         return sheets;
     }
 
+    public boolean print(int pages, int copies) {
+        int totalPages = pages * copies;
+        if (this.sheets >= totalPages) {
+            this.sheets -= totalPages;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getToner() {
+        return toner;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,11 +8,28 @@ public class PrinterTest {
 
     @Before
     public void before() {
-        printer = new Printer(20);
+        printer = new Printer(20, 100);
     }
 
     @Test
     public void hasSheets() {
         assertEquals(20, printer.getSheets());
+    }
+
+    @Test
+    public void hasEnoughSheets() {
+        assertEquals(true, printer.print(5, 2));
+        assertEquals(10, printer.getSheets());
+    }
+
+    @Test
+    public void doesNotHaveEnoughSheets() {
+        assertEquals(false, printer.print(10, 3));
+        assertEquals(20, printer.getSheets());
+    }
+
+    @Test
+    public void hasToner() {
+        assertEquals(100, printer.getToner());
     }
 }
